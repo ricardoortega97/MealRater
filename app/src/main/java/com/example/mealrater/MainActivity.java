619b6273acda2e7a6e39dcf5add8b536ad03ba17
Ricardo <ricardo.ortega97@outlook.com>
2024-01-24 03:47:55 -0500

    "Rate Button created"

--- app/src/main/java/com/example/mealrater/MainActivity.java
+++ app/src/main/java/com/example/mealrater/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.mealrater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ratePicker.SaveRateListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intiRateButton();
+    }
+
+    private void intiRateButton(){
+        Button rateButton = findViewById(R.id.rateButton);
+        rateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                ratePicker ratePickerDialog = new ratePicker();
+                ratePickerDialog.show(fm, "Rate");
+            }
+        });
+    }
+    @Override
+    public void didFinishedratePicker(RatingBar ratings) {
+
     }
 }
